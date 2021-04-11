1a7b3521e1cc635c76d8147a17fb71b407f8ac1c
Shamika Sims <shamika.sims@bryanuniversity.edu>
2021-04-11 15:23:50 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const {v4:uuidv4} = require('uuid');
+
+const PORT = 3000;
+
+app.use(express.json())
+
+let bountys=[
+
+    { firstName:'Stephanie',lastName:'Peters',living: 'no',bountyAmount: '25,000',type: 'sith', _id: uuidv4()},
+    { firstName:'Stevie',lastName:'Jackson',living: 'yes',bountyAmount: '125,000',type: 'sith', _id: uuidv4()},
+    { firstName:'Tony',lastName:'Williams',living: 'yes',bountyAmount: '55,000',type: 'jedi', _id: uuidv4()},
+];
+
+app.get('/bountys',(req, res) => {
+    res.send(bountys)
+   })
+
+   app.get('/:bountyId',(req, res) => {
+       console.log(req.params)
+    const bountyId = req.params.bountyId;
+    const singularBounty = bounty.find (bounty => bounty._id === bountyId);
+     res.send(singularBounty);
+   })
+  
+app.post('/bountys',(req, res) => {
+    const newBounty = req.body;
+    newBounty._id
+    bountys.push(newBounty);
+    res.send('new bounty added')
+
+    console.log(bountys)
+    res.send(`successfully added ${newBounty. firstName} to the database`)
+})
+
+ app.listen(PORT,() => {
+    console.log(`App started on port:${PORT}`)
+    })
